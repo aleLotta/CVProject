3f479bdc9d1d2fa91bdaa00e7d891f4df35cfd38
Ale <alessandro.lotta@studenti.unipd.it>
2022-07-23 16:44:58 +0200

    begin with IoU and edited segmentation file

--- Code/Code/main.cpp
+++ Code/Code/main.cpp
@@ -1,162 +1,192 @@
-/*import numpy as np
-
-net = cv2.dnn.readNet('yolov3_training_last.weights', 'yolov3_testing.cfg')
-
-classes = []
-with open("classes.txt", "r") as f:
-    classes = f.read().splitlines()
-
-#cap = cv2.VideoCapture('video4.mp4')
-#cap = 'test_images/<your_test_image>.jpg'
-font = cv2.FONT_HERSHEY_PLAIN
-colors = np.random.uniform(0, 255, size=(100, 3))
-
-while True:
-    #_, img = cap.read()
-    #img = cv2.imread("test_images/<your_test_image>.jpg")
-    img = cv2.imread("30.jpg")
-    height, width, _ = img.shape
-
-    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
-    net.setInput(blob)
-    output_layers_names = net.getUnconnectedOutLayersNames()
-    layerOutputs = net.forward(output_layers_names)
-
-    boxes = []
-    confidences = []
-    class_ids = []
-
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.2:
-                center_x = int(detection[0]*width)
-                center_y = int(detection[1]*height)
-                w = int(detection[2]*width)
-                h = int(detection[3]*height)
-
-                x = int(center_x - w/2)
-                y = int(center_y - h/2)
-
-                boxes.append([x, y, w, h])
-                confidences.append((float(confidence)))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
-
-    if len(indexes)>0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            confidence = str(round(confidences[i],2))
-            color = colors[i]
-            cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
-            cv2.putText(img, label + " " + confidence, (x, y+20), font, 1, (255,255,255), 2)
-
-    #cv2.imshow('Image', img)
-    cv2.imwrite("Image.jpg", img)
-    break
-#cap.release()
-cv2.destroyAllWindows()*/
-
+/*** 1) Libraries and Namespaces ***/
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/dnn/dnn.hpp>
-#include <typeinfo>
-
+#include <fstream>
 using namespace cv;
 using namespace std;
 using namespace cv::dnn;
 
-vector<String> getOutputsNames(const Net& net);
 
-int main(int argc, char** argv)
+/*** 2) Global Parameters ***/
+
+const string CLASS_NAME = "hand";
+// costants
+const float INPUT_WIDTH = 640.0;
+const float INPUT_HEIGHT = 640.0;
+const float SCORE_THRESHOLD = 0.5;
+const float NMS_THRESHOLD = 0.45;
+const float CONFIDENCE_THRESHOLD = 0.45;
+// text
+const float FONT_SCALE = 0.7;
+const int FONT_FACE = FONT_HERSHEY_SIMPLEX;
+const int THICKNESS = 1;
+// colors
+Scalar BLACK = Scalar(0,0,0);
+Scalar BLUE = Scalar(255, 178, 50);
+Scalar YELLOW = Scalar(0, 255, 255);
+Scalar RED = Scalar(0,0,255);
+
+
+float boundingBoxes_IoU(vector<Rect> predBox, vector<Rect> gTBox);
+
+/*** 3) Draw Label ***/
+
+void draw_label(Mat& input_image, string label, int left, int top)
 {
-    //Load Yolo Model
-    cv::dnn::Net net = cv::dnn::readNet("../../../Model/yolov3_training_last.weights", "../../../Model/yolov3_testing.cfg", "Darknet");
-    //auto net = cv::dnn::readNet("yolov3_training_last.weights", "yolov3_testing.cfg", "Darknet");
-    //cv::dnn::Net net = cv::dnn::readNetFromDarknet("yolov3.cfg", "yolov3.weights");
-    
-    net.setPreferableBackend(DNN_BACKEND_OPENCV);
-    net.setPreferableTarget(DNN_TARGET_CPU);
-    
-    String classes = {"Hands"};
-    
-    Mat img = imread("../../Dataset progetto CV - Hand detection _ segmentation/rgb/01.jpg");
-    
-    
-    Mat blob = blobFromImage(img, 1/255, cv::Size(416,416), Scalar(0,0,0), true, false);
-    //Mat blob = blobFromImage(img, 0.01, Size(224, 224), Scalar(104, 117, 123));
-    //blobFromImage(img, blob, 1/255, cv::Size(416,416), Scalar(0,0,0), true, false);
-    
-    
-    net.setInput(blob);
-    
-    vector<String> output_layers_names = getOutputsNames(net);
-    
-    vector<Mat> outs;
-    
-    net.forward(outs, output_layers_names);
-    
-    vector<Rect> boxes;
-    vector<float> confidences;
-    vector<int> classIds;
-    
-    Mat temp;
-    
-    
-    for (size_t i = 0; i < outs.size(); ++i){
-      
-      float* data = (float*)outs[i].data;
-      for (int j = 0; j<outs[i].rows; ++j){
-        //cout<<typeid(data).name()<<endl;
-        
-        //Mat detection = outs[i].row(j);
-        Mat detection = outs[i];
-        //cout<<detection<<"**"<<endl;
-        
-        //Mat scores = detection.col(5);
-        Point classId;
-        float confidence = detection.at<float>(j,5);        
-        //cout<<confidence<<endl;
-        
-        //minMaxLoc(scores, NULL, &confidence, NULL, &classId);
-        if (confidence > 0){
-          cout<<confidence<<endl;
-        }
-        
-        //cout<<detection.col(5)<<endl;
-        temp.push_back(detection.col(5));
-        
-        /*if (detection.col(5)Mat::zeros(1,1,CV_64F)) {
-          cout<<detection.col(5)<<endl;
-        }*/
-      }
-    }
-    cout<<sum(temp)<<endl;
-    
-    return 0;
+    // Display the label at the top of the bounding box
+    int baseLine;
+    Size label_size = getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
+    top = max(top, label_size.height);
+    // Top left corner
+    Point tlc = Point(left, top);
+    // Bottom right corner
+    Point brc = Point(left + label_size.width, top + label_size.height + baseLine);
+    // Draw white rectangle
+    rectangle(input_image, tlc, brc, BLACK, FILLED);
+    // Put the label on the black rectangle
+    putText(input_image, label, Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
 }
 
-// Get the names of the output layers
-vector<String> getOutputsNames(const Net& net){
-      
-  static vector<String> names;
-  if (names.empty()){
-    //Get the indices of the output layers, i.e. the layers with unconnected outputs
-    vector<int> outLayers = net.getUnconnectedOutLayers();
-    
-    //get the names of all the layers in the network
-    vector<String> layersNames = net.getLayerNames();
-    
-    // Get the names of the output layers in names
-    names.resize(outLayers.size());
-    for (size_t i = 0; i < outLayers.size(); ++i)
-    names[i] = layersNames[outLayers[i] - 1];
+
+/*** 4) Pre-Processing ***/
+
+vector<Mat> pre_process(Mat &input_image, Net &net)
+{
+    // Convert to blob
+    Mat blob;
+    blobFromImage(input_image, blob, 1./255., Size(INPUT_WIDTH, INPUT_HEIGHT), Scalar(), true, false);
+
+    net.setInput(blob);
+
+    // Forward propagate
+    vector<Mat> outputs;
+    net.forward(outputs, net.getUnconnectedOutLayersNames());
+
+    return outputs;
+}
+
+
+/*** 5) Post-Processing ***/
+
+Mat post_process(Mat &input_image, vector<Mat> &outputs, vector<Rect> &boxes)
+{
+    // Only 1 class id (0: hand)
+    Point class_id;
+    class_id.x = 0;
+    class_id.y = 0;
+    // Initialize vectors to hold respective outputs while unwrapping detections
+    vector<float> confidences;
+    //vector<Rect> boxes;
+    // Resizing factor
+    float x_factor = input_image.cols / INPUT_WIDTH;
+    float y_factor = input_image.rows / INPUT_HEIGHT;
+    float *data = (float *)outputs[0].data;
+    const int dimensions = 6;
+	
+    /* A. Filter Good Detections */
+
+    // 25200 for default size 640
+    const int rows = 25200;
+    // Iterate through 25200 detections
+    for (int i = 0; i < rows; ++i)
+    {
+        float confidence = data[4];
+        // Discard bad detections and continue
+        if (confidence >= CONFIDENCE_THRESHOLD)
+        {
+            // Store class ID and confidence in the pre-defined respective vectors
+            confidences.push_back(confidence);
+            // Center
+            float cx = data[0];
+            float cy = data[1];
+            // Box dimension
+            float w = data[2];
+            float h = data[3];
+            // Bounding box coordinates
+            int left = int((cx - 0.5 * w) * x_factor);
+            int top = int((cy - 0.5 * h) * y_factor);
+            int width = int(w * x_factor);
+            int height = int(h * y_factor);
+            // Store good detections in the boxes vector
+            boxes.push_back(Rect(left, top, width, height));
+        }
+        // Jump to the next row
+        data += dimensions;
     }
-  return names;
+
+    /* B. Remove Overlapping Boxes */
+
+    // Perform Non-Maximum Suppression and draw predictions
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD, indices);
+    for (int i = 0; i < indices.size(); i++)
+    {
+        int idx = indices[i];
+        Rect box = boxes[idx];
+        int left = box.x;
+        int top = box.y;
+        int width = box.width;
+        int height = box.height;
+        // Draw bounding box
+        rectangle(input_image, Point(left, top), Point(left + width, top + height), BLUE, 3*THICKNESS);
+        // Get the label for the class name and its confidence
+        string label = format("%.2f", confidences[idx]);
+        // label = CLASS_NAME + ":" + label;
+        // Draw class labels
+        draw_label(input_image, label, left, top);
+    }
+    return input_image;
+}
+
+
+/*** 6) Main Function ***/
+
+int main()
+{
+    // Load image
+    Mat frame;
+    vector<cv::String> fn;
+    glob("../../Dataset progetto CV - Hand detection _ segmentation/rgb/*.jpg", fn, false);
+
+    vector<Mat> images;
+    
+    // Load model
+    Net net;
+    net = readNet("../../best.onnx");
+    // Process the image
+    vector<Mat> detections;
+    
+    size_t count = fn.size(); //number of png files in images folder
+    for (size_t i=0; i<count; i++){
+        frame = imread(fn[i]);
+        detections = pre_process(frame, net);
+        Mat frame_copy; frame.copyTo(frame_copy); // deep copy
+        vector<Rect> boxes;
+        Mat img = post_process(frame_copy, detections, boxes);
+        imshow("Output", img);
+        waitKey(0);
+    }
+    
+    //string path = "../../Dataset progetto CV - Hand detection _ segmentation/rgb/02.jpg";
+    // string path = "val/images/2.jpg";
+    //frame = imread(path);
+    
+    
+    // read groundTruth
+    vector<cv::String> fn;
+    glob("../../Dataset progetto CV - Hand detection _ segmentation/det/*.txt", fn, false);
+    for (size_t i = 0; i < fn.size(); i++){
+      newfile.open(fn[i],ios::in); //open a file to perform read operation using file object
+      if (newfile.is_open()){ //checking whether the file is open
+        string tp;
+        while(getline(newfile, tp)){ //read data from file object and put it into string.
+          cout << tp << "\n"; //print the data of the string
+        }
+        newfile.close(); //close the file object.
+      }
+    }
+    
+    return 0;
 }
 
 float boundingBoxes_IoU(vector<Rect> predBox, vector<Rect> gTBox){
--- Code/Code/segmentation.cpp
+++ Code/Code/segmentation.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/opencv.hpp>
+/*#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
@@ -30,19 +30,15 @@ int main(int argc, char** argv)
   //Mat blur = Mat::zeros(img.rows, img.cols, img.type());
   Mat gray_box;
   cvtColor( box, gray_box, COLOR_BGR2GRAY );
-  /*Mat blur;
-  bilateralFilter(gray_box, blur, 5, 200, 200);
-  imshow("Prova1", blur);
-  waitKey(0);*/
   
   //rectangle 
-  Mat rectangle = Mat::zeros(h, w, gray_box.type());
+  Mat rectangle = Mat::zeros(h, w, box.type());
   int i = 0;
   int j = 0;
   for (int k = x; k < w+x; k++){
     j = 0;
     for (int z = y; z < h+y; z++){
-        rectangle.at<uchar>(j,i) = gray_box.at<uchar>(z,k);
+        rectangle.at<Vec3b>(j,i) = box.at<Vec3b>(z,k);
         j++;
     }
     i++;
@@ -51,14 +47,203 @@ int main(int argc, char** argv)
   imshow("rect", rectangle);
   waitKey(0);
   
+  Mat blur;
+  bilateralFilter(rectangle, blur, 9, 150, 150);
+  imshow("Prova1", blur);
+  waitKey(0);
+  
+  
+  
   //Otsu
+  Mat gray_rect;
+  cvtColor( rectangle, gray_rect, COLOR_BGR2GRAY );
+  
   Mat thresh;
-  long double thres = cv::threshold(rectangle, thresh, 0,255, THRESH_OTSU);
+  //long double thres = cv::threshold(rectangle, thresh, 0,255, THRESH_OTSU);
+  long double thres = cv::threshold(gray_rect, thresh, 0,255, THRESH_OTSU);
   
   imshow("Prova2", thresh);
   waitKey(0);
-  //Canny edge
+  
+  
+  //Try with Morphological operators or findContours and drawContours
   //
   
   return 0;
+}*/
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+Mat src_gray;
+int thresh = 100;
+RNG rng(12345);
+
+void thresh_callback(int, void* );
+
+Mat K_Means(Mat Input, int K) {
+	Mat samples(Input.rows * Input.cols, Input.channels(), CV_32F);
+	for (int y = 0; y < Input.rows; y++)
+		for (int x = 0; x < Input.cols; x++)
+			for (int z = 0; z < Input.channels(); z++)
+				if (Input.channels() == 3) {
+					samples.at<float>(y + x * Input.rows, z) = Input.at<Vec3b>(y, x)[z];
+				}
+				else {
+					samples.at<float>(y + x * Input.rows, z) = Input.at<uchar>(y, x);
+				}
+
+	Mat labels;
+	int attempts = 10;
+	Mat centers;
+  TermCriteria criteria = TermCriteria(TermCriteria::EPS+TermCriteria::COUNT, 10, 1.0);
+	kmeans(samples, K, labels, criteria, attempts, KMEANS_PP_CENTERS, centers);
+
+
+	Mat new_image(Input.size(), Input.type());
+	for (int y = 0; y < Input.rows; y++)
+		for (int x = 0; x < Input.cols; x++)
+		{
+			int cluster_idx = labels.at<int>(y + x * Input.rows, 0);
+			if (Input.channels()==3) {
+				for (int i = 0; i < Input.channels(); i++) {
+					new_image.at<Vec3b>(y, x)[i] = centers.at<float>(cluster_idx, i);
+				}
+			}
+			else {
+				new_image.at<uchar>(y, x) = centers.at<float>(cluster_idx, 0);
+			}
+		}
+	//imshow("clustered image", new_image);
+	return new_image;
+}
+
+int main(int argc, char** argv)
+{
+  int x = 631;
+  int y = 318;
+  int w = 217;
+  int h = 122;
+ 
+  /*int x = 89;
+  int y = 90;
+  int w = 68;
+  int h = 69;*/
+  
+  Mat img = imread("01.jpg");
+  cout<<img.size()<<endl;
+ 
+  Mat box = Mat::zeros(img.rows, img.cols, img.type());
+  cout<<box.size()<<endl;
+ 
+  for (int k = x; k < w+x; k++){
+    for (int z = y; z < h+y; z++){
+        box.at<Vec3b>(z,k) = img.at<Vec3b>(z,k);
+    }
+  }
+ 
+  imshow("Prova", box);
+  //waitKey(0);
+ 
+ 
+  //bilateralFilter
+  //Mat blur = Mat::zeros(img.rows, img.cols, img.type());
+  Mat gray_box;
+  cvtColor( box, gray_box, COLOR_BGR2GRAY );
+ 
+  //rectangle
+  Mat rectangle = Mat::zeros(h, w, box.type());
+  int i = 0;
+  int j = 0;
+  for (int k = x; k < w+x; k++){
+    j = 0;
+    for (int z = y; z < h+y; z++){
+        rectangle.at<Vec3b>(j,i) = box.at<Vec3b>(z,k);
+        j++;
+    }
+    i++;
+  }
+ 
+  imshow("rect", rectangle);
+  waitKey(0);
+ 
+  /*Mat blur;
+  bilateralFilter(rectangle, blur, 9, 150, 150);
+  imshow("Prova1", blur);
+  waitKey(0);*/
+ 
+ 
+ 
+  //Otsu
+  Mat gray_rect;
+  cvtColor( rectangle, gray_rect, COLOR_BGR2GRAY );
+
+  gray_rect.copyTo(src_gray);
+  
+  //GaussianBlur( src_gray, src_gray, Size(9,9), 0);
+  /*Mat clusteredImg = K_Means(src_gray, 3);
+  imshow("clusters", clusteredImg);
+  waitKey(0);*/
+ 
+  //Mat thresh;
+  Mat temp_img, dst_img, t_img;
+  
+  dst_img = Mat::zeros(gray_rect.size(), gray_rect.type());
+  t_img = Mat::zeros(gray_rect.size(), gray_rect.type());
+  
+  int rect_h = floor(rectangle.rows/2);
+  int rect_w = floor(rectangle.cols/3);
+
+  for (int k = 0;k < 3; k++){
+    for (int g = 0; g < 2; g++) {
+      temp_img = Mat::zeros(gray_rect.size(), gray_rect.type());
+      for (int i = g*rect_w; i < rect_w*(g+1); i++){
+        for (int j = k*rect_h; j < rect_h*(k+1); j++){
+          temp_img.at<uchar>(i,j) = 255;
+        }
+      }
+      gray_rect.copyTo(temp_img, temp_img);
+      
+      //GaussianBlur(temp_img,t_img,Size(9,9),0);
+      bilateralFilter(temp_img, t_img, 5, 75, 75);
+      threshold(t_img, t_img, 0, 255, THRESH_OTSU);
+      
+      dst_img = dst_img + t_img;
+    }
+  } 
+  imshow("Otsu", dst_img);
+  waitKey(0);
+ 
+ 
+  //Try with Morphological operators or findContours and drawContours
+  /*const int max_thresh = 255;
+  const char* source_window = "Source";
+  namedWindow(source_window);
+  createTrackbar( "Canny thresh:", source_window, &thresh, max_thresh, thresh_callback );
+  thresh_callback(0,0);
+  waitKey();*/
+
+  return 0;
+}
+
+void thresh_callback(int, void* )
+{
+    Mat canny_output;
+    //Canny( src_gray, canny_output, thresh, thresh*2 );
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    //findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
+    findContours( src_gray, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE );
+    //Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    threshold(src_gray, canny_output, 200, 255, THRESH_BINARY);
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256) );
+        drawContours( drawing, contours, (int)i, color, FILLED, 8, hierarchy);
+        //drawContours( drawing, contours, (int)i, color, 2, LINE8, hierarchy);
+    }
+    imshow( "Contours", drawing );
 }
